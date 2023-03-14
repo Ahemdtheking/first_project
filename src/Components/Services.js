1b6d0be03ad66f425c6f1da9d471853e86c31454
Ahmed Mahim <89526416+Ahemdtheking@users.noreply.github.com>
2023-03-14 23:39:53 +0600

    Update Services.js

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -4,7 +4,7 @@ const Services = () => {
   return (
     <div>
       <h1 className='DT'>Welcome to Services Page</h1>
-    <p>There is nothing to serv!<p/>
+    <p>There is nothing to serv!</p>
     </div>
   )
 }
