14f8399cb4e84a33e31a1493fc146bfccba19f25
ahmed mahim <ahmed.mahim2007@gmai.com>
2023-03-14 20:28:54 +0600

    firstproject

--- src/Components/Error Page.js
+++ src/Components/Error Page.js
@@ -3,7 +3,8 @@ import React from 'react'
 const ErrorPage = () => {
   return (
     <div>
-        <h1>Error 404 Page Found :)</h1>
+        <h1>Error 404 Page Not Found :(</h1>
+        <p>Your are unble to wrong page</p>
     </div>
   )
 }
